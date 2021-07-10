14d4d6f92f1415829e5aa4dd303e592acefaf2dd
Raphael Amoedo <avalon.ralph@gmail.com>
2021-07-10 19:14:14 +0000

    Add backend initial structure

---
+++ backend/src/main/java/com/featuretoggle/FeaturetoggleApplication.java
@@ -0,0 +1,13 @@
+package com.featuretoggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturetoggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturetoggleApplication.class, args);
+	}
+
+}
